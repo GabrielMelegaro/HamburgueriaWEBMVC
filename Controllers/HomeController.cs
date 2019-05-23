4ffee01fb1262980712f6266815f21c259592cd1
Gabriel Melegaro De Oliveira <47218167829@info.local>
2019-05-23 17:12:40 -0300

    HamburgueriaCommit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Hamburgueria_Tarde.Models;
+
+namespace Hamburgueria_Tarde.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        { 
+            ViewData["NomeView"] = "Home";
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
